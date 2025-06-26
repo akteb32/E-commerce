2b4983d04c175efa5b00dca4127cbfb0af0a983b
Abdalrahman Khateeb <akteb32@gmail.com>
2025-06-26 03:23:27 +0300

    create coupon pages for the backend

--- resources/views/backend/pages/coupon/create.blade.php
+++ resources/views/backend/pages/coupon/create.blade.php
@@ -4,7 +4,59 @@
 
 
 
-    
+    <form method="POST" action="{{route('coupon.store')}}">
+        @CSRF
+        <div class="form-group card-body">
+            <div class="card">
+
+                <div class="form-group">
+                    <label>Coupon name</label>
+                    <input type="text" class="form-control" placeholder="Coupon name" name="coupon_name"
+                        value="{{$coupon->name}}" required>
+                </div>
+                <div class="form-group">
+                    <label>coupon code</label>
+                    <input type="text" class="form-control" placeholder="coupon code" name="coupon_code"
+                        value="{{$coupon->code}}" required>
+                </div>
+                <div class="form-group">
+                    <label>coupon type</label>
+                    <input type="text" class="form-control" placeholder="Enter coupon type" name="coupon_type" required>
+                </div>
+                <div class="form-group">
+                    <label>coupon value</label>
+                    <input type="text" class="form-control" placeholder="Enter coupon value" name="coupon_value" required>
+                </div>
+                <div class="form-group">
+                    <label>coupon minimum_amount</label>
+                    <input type="text" class="form-control" placeholder="Enter minimum_amount" name="coupon_minimum_amount"
+                        required>
+                </div>
+                <div class="form-group">
+                    <label>coupon usage_limit</label>
+                    <input type="text" class="form-control" placeholder="Enter usage_limit" name="coupon_usage_limit"
+                        required>
+                </div>
+                <div class="form-group">
+                    <label>coupon used_count</label>
+                    <input type="text" class="form-control" placeholder="Enter used_count" name="coupon_used_count"
+                        required>
+                </div>
+
+
+
+
+
+
+
+
+
+
+            </div>
+        </div>
+
+        <button type="submit" class="btn btn-primary mt-3">Submit</button>
+    </form>
 
 
 
--- resources/views/backend/pages/coupon/edit.blade.php
+++ resources/views/backend/pages/coupon/edit.blade.php
@@ -1,15 +1,57 @@
 @extends('layout.backend.backmaster')
 
-    @section('backend_main')
-
-
-    
-
-
-  
+@section('backend_main')
 
 
 
 
 
-    @endsection
+
+    <form method="POST" action="{{route('coupon.update', $coupon->id)}}">
+        @CSRF
+        @method('PUT')
+        <div class="form-group">
+            <label>Coupon name</label>
+            <input type="text" class="form-control" placeholder="Coupon name" name="coupon_name" value="{{$coupon->name}}"
+                required>
+        </div>
+        <div class="form-group">
+            <label>coupon code</label>
+            <input type="text" class="form-control" placeholder="coupon code" name="coupon_code" value="{{$coupon->code}}"
+                required>
+        </div>
+        <div class="form-group">
+            <label>coupon type</label>
+            <input type="text" class="form-control" placeholder="Enter coupon type" name="coupon_type"
+                value="{{$coupon->type}}" required>
+        </div>
+        <div class="form-group">
+            <label>coupon value</label>
+            <input type="text" class="form-control" placeholder="Enter coupon value" name="coupon_value"
+                value="{{$coupon->value}}" required>
+        </div>
+        <div class="form-group">
+            <label>coupon minimum_amount</label>
+            <input type="text" class="form-control" placeholder="Enter minimum_amount" name="coupon_minimum_amount"
+                value="{{$coupon->minimum_amount}}" required>
+        </div>
+        <div class="form-group">
+            <label>coupon usage_limit</label>
+            <input type="text" class="form-control" placeholder="Enter usage_limit" name="coupon_usage_limit"
+                value="{{$coupon->usage_limit}}" required>
+        </div>
+        <div class="form-group">
+            <label>coupon used_count</label>
+            <input type="text" class="form-control" placeholder="Enter used_count" name="coupon_used_count"
+                value="{{$coupon->used_count}}" required>
+        </div>
+
+
+        <button type="submit" class="btn btn-primary mt-3">Submit</button>
+    </form>
+
+
+
+
+
+@endsection
