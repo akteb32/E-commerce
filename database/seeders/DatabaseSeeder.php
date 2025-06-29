c2487e992ed334dc94757b4e79a7d6360c73651b
Abdalrahman Khateeb <akteb32@gmail.com>
2025-06-29 10:29:01 +0300

    Add assets folder for the backend

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Role;
 use App\Models\User;
+use Hash;
 use Illuminate\Database\Seeder;
 use PhpParser\Builder\Use_;
 
